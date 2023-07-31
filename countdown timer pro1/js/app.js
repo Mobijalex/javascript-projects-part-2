6b2c0fe782dbf373ff1675b8c2678b386475f02a
mobijalex <mobijalex6@gmail.com>
2023-07-31 17:55:14 +0530

    finished

--- countdown timer pro1/js/app.js
+++ countdown timer pro1/js/app.js
@@ -9,9 +9,13 @@ function clock() {
   //   convert into days
   const diff = (end - now) / 1000;
   if (diff < 0) return;
+  //   days
   inputs[0].value = Math.floor(diff / 3600 / 24);
+  // hours
   inputs[1].value = Math.floor((diff / 3600) % 24);
+  // minutes
   inputs[2].value = Math.floor((diff / 60) % 60);
+  //   seconds
   inputs[3].value = Math.floor(diff % 60);
 }
 
