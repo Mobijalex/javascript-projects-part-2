f165ca35992cc4112cfcda1c5d37b373dc05c513
mobijalex <mobijalex6@gmail.com>
2023-08-02 17:45:49 +0530

    password generator done

---
+++ password genrator pro4/js/app.js
@@ -0,0 +1,52 @@
+const upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerSet = "abcdefghijklmopqrstuvwxyz";
+const numberSet = "1234567890";
+const symbolSet = "!@#$%^&*()_+";
+
+// selectors
+const passBox = document.getElementById("pass-box");
+const totalChar = document.getElementById("total-char");
+const upperInput = document.getElementById("upper-case");
+const lowerInput = document.getElementById("lower-case");
+const numberInput = document.getElementById("numbers");
+const symbolInput = document.getElementById("symbols");
+
+// random data generation function
+const getRandomData = (dataSet) => {
+  return dataSet[Math.floor(Math.random() * dataSet.length)];
+};
+
+const generatePassword = (password = "") => {
+  if (upperInput.checked) {
+    password += getRandomData(upperSet);
+  }
+  if (lowerInput.checked) {
+    password += getRandomData(lowerSet);
+  }
+  if (numberInput.checked) {
+    password += getRandomData(numberSet);
+  }
+  if (symbolInput.checked) {
+    password += getRandomData(symbolSet);
+  }
+
+  //   this can be called as recurssive function
+  if (password.length < totalChar.value) {
+    return generatePassword(password);
+  }
+  passBox.innerText = turncateString(password, totalChar.value);
+};
+
+document.getElementById("btn").addEventListener("click", function () {
+  generatePassword();
+});
+
+function turncateString(str, num) {
+  if (str.length > num) {
+    console.log(str);
+    let substr = str.substring(0, num);
+    return substr;
+  } else {
+    return str;
+  }
+}
